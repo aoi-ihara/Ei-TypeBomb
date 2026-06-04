b7908d52efdfa9aebfe5ad89c9f265a41c51de65
vgnz93hs <ilce6400.mail@icloud.com>
2026-06-04 22:41:56 +0900

    fix: separate server and client

--- client/app/not-found.tsx
+++ client/app/not-found.tsx
@@ -1,6 +1,4 @@
-"use client";
-
-import { useEffect, useState } from "react";
+import BlinkText from "@/app/NotFoundClient";
 import { Metadata } from "next";
 
 export const metadata: Metadata = {
@@ -8,37 +6,5 @@ export const metadata: Metadata = {
 };
 
 export default function Loading() {
-    const [showCursor, setShowCursor] = useState(true);
-
-    useEffect(() => {
-        const intervalId = setInterval(() => {
-            setShowCursor((prev) => !prev);
-        }, 500);
-
-        return () => {
-            clearInterval(intervalId);
-        };
-    }, []);
-
-    return (
-        <main className="flex flex-col h-full items-center pt-16">
-            <h1 className="font-mono font-bold text-2xl" data-cursor="text">
-                404
-            </h1>
-            <div className="flex items-end">
-                <h2
-                    className="font-mono font-bold mt-4 text-lg"
-                    data-cursor="text"
-                >
-                    Not Found
-                </h2>
-                <div
-                    className={`w-3 h-1 mb-1 ml-1 bg-cyan-600 ${!showCursor && "opacity-0"}`}
-                />
-                <button className="ml-4 px-4 py-2 bg-cyan-600 text-white">
-                    Go Home
-                </button>
-            </div>
-        </main>
-    );
+    return <BlinkText />;
 }
