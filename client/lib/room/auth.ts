abddeba9f528d8af5d7787104b8651d12d35e914
vgnz93hs <ilce6400.mail@icloud.com>
2026-07-24 20:08:11 +0800

    fix: jsonwebtoken から jose に変更。

--- client/lib/room/auth.ts
+++ client/lib/room/auth.ts
@@ -5,14 +5,20 @@ import type { Room } from "@/type";
 import isUUID from "validator/es/lib/isUUID";
 import { validatePassword } from "../auth/validator";
 import { verifyTurnstile } from "../auth/turnstile";
-import jwt from "jsonwebtoken";
+import { SignJWT, jwtVerify } from "jose";
 import { cookies } from "next/headers";
+
+export const getCookieValue = async () => {
+    const cookieStore = await cookies();
+    const authToken = cookieStore.get("auth-token")?.value;
+    return authToken;
+};
 
 export const signInToRoom = async (room: Room, turnstileToken?: string) => {
     if (!isUUID(room.id, 4)) return "Incorrect Room ID.";
 
     if (room.password) {
-        if (validatePassword(room.password)) return "Incorrect password.";
+        if (!validatePassword(room.password)) return "Incorrect password.";
 
         if (!turnstileToken) return "Turnstile token is required.";
         const turnstileResult = await verifyTurnstile(turnstileToken);
@@ -25,14 +31,11 @@ export const signInToRoom = async (room: Room, turnstileToken?: string) => {
             .eq("id", room.id)
             .maybeSingle();
 
-        if (error) {
-            return error.message;
-        }
+        if (error) return error.message;
         if (!data?.password) return "Cannot get the password.";
-
         if (data.password !== room.password) return "Incorrect password.";
 
-        setAuthCookie(room.id);
+        await setAuthCookie(room.id);
     } else {
         const supabase = await createAdminClient();
         const { data, error } = await supabase
@@ -41,34 +44,25 @@ export const signInToRoom = async (room: Room, turnstileToken?: string) => {
             .eq("id", room.id)
             .maybeSingle();
 
-        if (error) {
-            return error.message;
-        }
+        if (error) return error.message;
+        if (!data) return "Room not found.";
+        if (data.password) return "Password is required.";
 
-        if (!data) {
-            return "Room not found.";
-        }
-
-        if (data.password) {
-            return "Password is required.";
-        }
-
-        setAuthCookie(room.id);
+        await setAuthCookie(room.id);
     }
 
     return null;
 };
 
 const setAuthCookie = async (id: string) => {
-    const token = jwt.sign(
-        {
-            id,
-        },
-        process.env.JWT_SECRET!,
-        {
-            expiresIn: "6h",
-        },
-    );
+    const encoder = new TextEncoder();
+    const JWT_SECRET = encoder.encode(process.env.JWT_SECRET!);
+
+    const token = await new SignJWT({ id })
+        .setProtectedHeader({ alg: "HS256" })
+        .setIssuedAt()
+        .setExpirationTime("4h")
+        .sign(JWT_SECRET);
 
     const cookieStore = await cookies();
 
@@ -76,7 +70,7 @@ const setAuthCookie = async (id: string) => {
         httpOnly: true,
         secure: process.env.NODE_ENV === "production",
         sameSite: "lax",
-        maxAge: 60 * 60 * 6,
+        maxAge: 60 * 60 * 4,
         path: "/",
     });
 };
