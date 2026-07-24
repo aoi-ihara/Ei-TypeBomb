8f359d975631032bdb7a957ef268bfccc96d7498
vgnz93hs <ilce6400.mail@icloud.com>
2026-07-24 16:55:54 +0800

    フォントが適応されないバグを修正。

--- client/app/layout.tsx
+++ client/app/layout.tsx
@@ -2,6 +2,12 @@ import type { Metadata } from "next";
 import "./globals.css";
 import Cursor from "@/components/ui/Cursor";
 import { Footer } from "@/components/layout/Footer";
+import { LINE_Seed_JP } from "next/font/google";
+
+const lineSeedJp = LINE_Seed_JP({
+    weight: ["400", "700"],
+    variable: "--font-line-seed-jp",
+});
 
 export const metadata: Metadata = {
     title: "Ei-TypeBomb - vgnz93hs",
@@ -23,8 +29,10 @@ export default function RootLayout({
     children: React.ReactNode;
 }>) {
     return (
-        <html lang="en">
-            <body className="min-h-full flex flex-col">
+        <html lang="ja">
+            <body
+                className={`${lineSeedJp.className} min-h-full flex flex-col`}
+            >
                 <main className="flex flex-col h-dvh w-full items-center">
                     {children}
                     <div className="text-xs w-full flex fixed left-4 opacity-50 justify-center md:justify-start bottom-3">
