0f926729b3b95008b9a234f00df72e751f715c3b
vgnz93hs <ilce6400.mail@icloud.com>
2026-07-26 11:00:51 +0800

    fix: アイコンが小さくなるバグを修正。

--- client/app/(editor)/my-rooms/page.tsx
+++ client/app/(editor)/my-rooms/page.tsx
@@ -57,13 +57,14 @@ export default function Profile() {
                                         router.push(`/my-rooms/${room.id}`)
                                     }
                                 >
-                                    <div className="flex gap-2 items-center">
+                                    <div className="flex shrink-0 gap-2 items-center">
                                         {room.password ? (
                                             <svg
                                                 xmlns="http://www.w3.org/2000/svg"
                                                 height="20px"
                                                 viewBox="0 -960 960 960"
                                                 width="20px"
+                                                className="shrink-0"
                                                 fill="currentColor"
                                             >
                                                 <path d="M240-80q-33 0-56.5-23.5T160-160v-400q0-33 23.5-56.5T240-640h40v-80q0-83 58.5-141.5T480-920q83 0 141.5 58.5T680-720v80h40q33 0 56.5 23.5T800-560v400q0 33-23.5 56.5T720-80H240Zm296.5-223.5Q560-327 560-360t-23.5-56.5Q513-440 480-440t-56.5 23.5Q400-393 400-360t23.5 56.5Q447-280 480-280t56.5-23.5ZM360-640h240v-80q0-50-35-85t-85-35q-50 0-85 35t-35 85v80Z" />
