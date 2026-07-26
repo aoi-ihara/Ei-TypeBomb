331e42f0164d3c61374ecdbca0144f4cadb924fc
vgnz93hs <ilce6400.mail@icloud.com>
2026-07-26 16:25:50 +0800

    feature: 404ページのデザインの変更。

--- client/app/NotFoundClient.tsx
+++ client/app/NotFoundClient.tsx
@@ -2,6 +2,8 @@
 
 import { useEffect, useState } from "react";
 import { useRouter } from "next/navigation";
+import Button from "@/components/ui/Button";
+import Shell from "@/components/layout/Shell";
 
 export default function NotFoundClient() {
     const router = useRouter();
@@ -18,13 +20,7 @@ export default function NotFoundClient() {
     }, []);
 
     return (
-        <main className="flex flex-col h-full items-center pt-16">
-            <h1
-                className="font-mono mb-8 font-bold text-2xl"
-                data-cursor="text"
-            >
-                404
-            </h1>
+        <Shell title="404" size="small">
             <div className="flex items-end">
                 <h2
                     className="font-mono font-bold mt-4 text-lg"
@@ -36,16 +32,18 @@ export default function NotFoundClient() {
                     className={`w-3 h-1 mb-1 ml-1 bg-cyan-600 ${!showCursor && "opacity-0"}`}
                 />
             </div>
-            <button
-                data-cursor="button"
+
+            <div data-cursor="text">
+                We could not find the page you are looking for.
+            </div>
+
+            <Button
+                className="w-full"
+                variant="primary"
                 onClick={() => router.push("/")}
-                data-cursor-shape="0"
-                className="mt-16 rounded-xl"
             >
-                <div className="w-48 font-bold rounded-xl transition-all duration-200 ease-out active:scale-95 py-3 bg-cyan-600 text-white">
-                    Go Home
-                </div>
-            </button>
-        </main>
+                Go Home
+            </Button>
+        </Shell>
     );
 }
