7e6d0f979e56520843139fc84cfebff2dbcef7c8
vgnz93hs <ilce6400.mail@icloud.com>
2026-07-26 16:13:38 +0800

    fix: フォントの変更。

--- client/app/layout.tsx
+++ client/app/layout.tsx
@@ -2,11 +2,18 @@ import type { Metadata } from "next";
 import "./globals.css";
 import Cursor from "@/components/ui/Cursor";
 import { Footer } from "@/components/layout/Footer";
-import { LINE_Seed_JP } from "next/font/google";
+import { LINE_Seed_JP, JetBrains_Mono } from "next/font/google";
 
 const lineSeedJp = LINE_Seed_JP({
+    subsets: ["latin"],
     weight: ["400", "700"],
     variable: "--font-line-seed-jp",
+});
+
+const jetbrainsMono = JetBrains_Mono({
+    subsets: ["latin"],
+    weight: ["400", "700"],
+    variable: "--jetbrains-mono",
 });
 
 export const metadata: Metadata = {
@@ -31,7 +38,7 @@ export default function RootLayout({
     return (
         <html lang="ja">
             <body
-                className={`${lineSeedJp.className} min-h-full flex flex-col`}
+                className={`${lineSeedJp.variable} ${jetbrainsMono.variable} min-h-full flex flex-col`}
             >
                 <main className="flex flex-col h-dvh w-full items-center">
                     {children}
