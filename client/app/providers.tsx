aec900949098c14926b2bb603203b94dd069d4e2
vgnz93hs <ilce6400.mail@icloud.com>
2026-07-28 12:49:28 +0800

    fix: Appプロパティの追加。

--- client/app/providers.tsx
+++ client/app/providers.tsx
@@ -5,6 +5,15 @@ import { useEffect, Suspense } from "react";
 import posthog from "posthog-js";
 import { PostHogProvider as PHProvider } from "posthog-js/react";
 
+if (typeof window !== "undefined" && !posthog.__loaded) {
+    posthog.init(process.env.NEXT_PUBLIC_POSTHOG_KEY!, {
+        api_host:
+            process.env.NEXT_PUBLIC_POSTHOG_HOST || "https://us.i.posthog.com",
+        capture_pageview: false,
+    });
+    posthog.register({ app: "ei-typebomb" });
+}
+
 function PostHogPageView() {
     const pathname = usePathname();
     const searchParams = useSearchParams();
