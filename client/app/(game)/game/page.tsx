1dfd24a4f40ba422e4830921e1de44839d6397c2
vgnz93hs <ilce6400.mail@icloud.com>
2026-09-15 12:36:56 +0900

    fix: インデントの修正。

--- client/app/(game)/game/page.tsx
+++ client/app/(game)/game/page.tsx
@@ -2,19 +2,19 @@ import Client from "./Client";
 import { cookies } from "next/headers";
 
 export default async function SettingsPage() {
-	const cookieStore = await cookies();
+    const cookieStore = await cookies();
 
-	const backgroundMusic =
-		cookieStore.get("background-music")?.value !== "false";
-	const sounDeffects = cookieStore.get("sound-effects")?.value !== "false";
+    const backgroundMusic =
+        cookieStore.get("background-music")?.value !== "false";
+    const sounDeffects = cookieStore.get("sound-effects")?.value !== "false";
 
-	const serverUrl = cookieStore.get("server-url")?.value ?? "";
+    const serverUrl = cookieStore.get("server-url")?.value ?? "";
 
-	return (
-		<Client
-			initialSounDeffects={sounDeffects}
-			initialBackgroundMusic={backgroundMusic}
-			initialServerUrl={serverUrl}
-		/>
-	);
+    return (
+        <Client
+            initialSounDeffects={sounDeffects}
+            initialBackgroundMusic={backgroundMusic}
+            initialServerUrl={serverUrl}
+        />
+    );
 }
