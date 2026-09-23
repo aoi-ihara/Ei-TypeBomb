b91287b58007c7dd19aa55967ee745c477ae4264
vgnz93hs <ilce6400.mail@icloud.com>
2026-09-23 18:34:18 +0900

    読み込み時の無駄な変数の削除。

--- client/app/loading.tsx
+++ client/app/loading.tsx
@@ -1,18 +1,7 @@
 "use client";
 
 import Shell from "@/components/layout/Shell";
-import { useEffect, useState } from "react";
 
 export default function Loading() {
-    const [showCursor, setShowCursor] = useState(true);
-
-    useEffect(() => {
-        const intervalId = setInterval(() => {
-            setShowCursor((prev) => !prev);
-        }, 500);
-
-        return () => clearInterval(intervalId);
-    }, []);
-
     return <Shell loading={true} />;
 }
